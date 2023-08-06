e523c582aff11b6362d26abbea8433fc74cf9411
SashaPotemkin <alex.potjomkin@gmail.com>
2023-08-06 17:09:30 +0300

    some changes

--- app/src/main/java/ru/potemkin/coroutineflow/MainActivity.kt
+++ app/src/main/java/ru/potemkin/coroutineflow/MainActivity.kt
@@ -1,11 +1,21 @@
 package ru.potemkin.coroutineflow
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.potemkin.coroutineflow.databinding.ActivityMainBinding
+import ru.potemkin.coroutineflow.Lesson2.UsersActivity
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        binding.buttonUsersActivity.setOnClickListener {
+            startActivity(UsersActivity.newIntent(this))
+        }
     }
 }
