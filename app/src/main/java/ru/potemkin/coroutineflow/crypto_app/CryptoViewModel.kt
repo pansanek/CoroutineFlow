90d79ce569e2e5a173d23c4702de0a159371253e
SashaPotemkin <alex.potjomkin@gmail.com>
2023-08-07 14:31:55 +0300

    Промежуточные и кастомные операторы

--- app/src/main/java/ru/potemkin/coroutineflow/crypto_app/CryptoViewModel.kt
+++ app/src/main/java/ru/potemkin/coroutineflow/crypto_app/CryptoViewModel.kt
@@ -12,6 +12,7 @@ class CryptoViewModel : ViewModel() {
 
     private val repository = CryptoRepository
 
+    private val loadingFlow = MutableSharedFlow<State>()
 
     val state: Flow<State> = repository.getCurrencyList()
         .filter { it.isNotEmpty() }
@@ -19,9 +20,15 @@ class CryptoViewModel : ViewModel() {
         .onStart {
             emit(State.Loading)
         }
+        .mergeWith(loadingFlow)
+
+    private fun Flow<State>.mergeWith(another: Flow<State>):Flow<State>{
+        return merge(this,another)
+    }
 
     fun refreshList() {
         viewModelScope.launch {
+            loadingFlow.emit(State.Loading)
             repository.refreshList()
         }
     }
